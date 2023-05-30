52bac7279aa614a101d79ea5271a50c4586c0b20
cemreb6 <eminecemrebitgen@hotmail.com>
2023-05-30 20:42:10 +0300

    app init

---
+++ src/utils/Constants.js
@@ -0,0 +1,10 @@
+export const BloodTypes={
+    "A_negative":"A-",
+    "A_positive":"A+",
+    "B_negative":"B-",
+    "B_positive":"B+",
+    "AB_negative":"AB-",
+    "AB_positive":"AB+",
+    "0_negative":"O-",
+    "O_Positive":"0+"
+}
