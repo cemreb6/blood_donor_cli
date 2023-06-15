ac9163f66bdc9cf0d08e28487cd8dc46a3c588d0
cemreb6 <84712496+cemreb6@users.noreply.github.com>
2023-06-15 22:18:11 +0300

    spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+export const Spinner = () => {
+    return (
+        <div id="backdrop">
+            <div className="text-center loading">
+                <div className="spinner-border" role="status" />
+            </div>
+        </div>
+    );
+}
