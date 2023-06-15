dc68ff4cf44815396f6b2e84bd8453eeab1917ca
cemreb6 <84712496+cemreb6@users.noreply.github.com>
2023-06-15 21:57:29 +0300

    api endpoints

--- src/pages/api/ApiRoutes.js
+++ src/pages/api/ApiRoutes.js
@@ -1,5 +1,16 @@
 const baseUrl="https://blooddonorapigateway20230611141343.azurewebsites.net/api/v1";
 
 const userBase=baseUrl.concat("/User");
+const donorBase=baseUrl.concat("/Donor");
+const bloodBase=baseUrl.concat("/BloodRequest");
 
-export const testUrl=userBase.concat("/Yey");
+export const loginHospitalUrl=userBase.concat("/LoginToHospital");
+export const loginBranchUrl=userBase.concat("/LoginToBranch");
+export const registerToBranchUrl=userBase.concat("/RegisterToBranch")
+export const registerToHospitalUrl=userBase.concat("/RegisterToHospital");
+
+export const addDonorUrl=donorBase.concat("/Add");
+export const findDonorUrl=donorBase.concat("/Find");
+export const donateUrl=donorBase.concat("/Donate");
+
+export const requestBloodUrl=bloodBase.concat("/RequestBlood");
